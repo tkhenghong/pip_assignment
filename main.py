87875fe7bc10a9afadf78c5f306168a41cbe6aa0
Teoh Kheng Hong <tkhenghong@gmail.com>
2021-10-10 15:56:39 +0800

    Added save customer function, display UI menus for different users, Optimized code, in progress...

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# TEOH KHENG HONG
+# TP030562
 
 import os
 
@@ -13,16 +11,68 @@ def string_is_blank(myString):
 
 # Read last line of file efficiently (Reference: https://stackoverflow.com/a/54278929)
 def get_last_line_of_file(file_name):
-    with open(file_name, 'rb') as f:
-        f.seek(-2, os.SEEK_END)
-        while f.read(1) != b'\n':
-            f.seek(-2, os.SEEK_CUR)
-        return f.readline().decode()
+    if os.path.isfile(file_name):
+        with open(file_name, 'rb') as f:
+            f.seek(-2, os.SEEK_END)
+            while f.read(1) != b'\n':
+                f.seek(-2, os.SEEK_CUR)
+            return f.readline().decode()
+    else:
+        return ''
 
 
-def save_user(username, password, user_type):
+def get_username(user_type, default_username):
+    while True:
+        customer_username = input('Please enter the' + user_type + '\'s username. ' + (
+            '(Default ' + user_type + '\'s' + ' username: ' + default_username + ')' if default_username else ''))
+        if string_is_blank(customer_username):
+            print('Username is empty.')
+            continue
+        else:
+            break
+    return customer_username
+
+
+def get_user_password(user_type, default_password):
+    while True:
+        password = input(
+            'Please enter the ' + user_type + '\'s password. ' + (
+                '(Default ' + user_type + '\'s' + ' password: ' + default_password + ')' if default_password else ''))
+        if string_is_blank(password):
+            if not default_password:
+                print(user_type + '\'s password is empty. Please try again')
+                continue
+            else:
+                password = default_password
+                break
+        else:
+            confirm_password = input('Confirm the ' + user_type + '\'s password: ')
+            if string_is_blank(confirm_password):
+                print('Please try again. Confirm the ' + user_type + '\'s password: ')
+            else:
+                if password != confirm_password:
+                    print('The passwords are not the same. Please enter the details again.')
+                else:
+                    break
+    return password
+
+
+def get_user_name(user_type):
+    while True:
+        name = input('Please enter the ' + user_type + '\'s name: ')
+        if string_is_blank(name):
+            print(user_type + '\'s name is empty.')
+            continue
+        else:
+            break
+    return name
+
+
+# ID \t username \t password \t name \t user_type
+# Save user (Admin/Customer) into User.txt file
+def save_user(username, password, name, user_type):
     print('Saving user info...')
-    user_file = open('User.txt', 'rb')
+
     last_user_line = get_last_line_of_file('User.txt')
 
     # Split the last line user info into an array of texts
@@ -34,69 +84,162 @@ def save_user(username, password, user_type):
     # Get the 1st information of the last_user_info array, then substring the text to take the 2nd character till end.
     # Convert the result to string and addition by 1 .
     # Reference: https://www.freecodecamp.org/news/how-to-substring-a-string-in-python/
-    new_user_id = (int(last_user_info[0][1:-1]) + 1)
+    existing_user_id = last_user_info[0][1:-1] if string_is_blank(last_user_info[0]) else '0'
+    new_user_id = 1 if string_is_blank(last_user_info[0]) else int(existing_user_id) + 1
 
-    user_file.write('U' + str(new_user_id) + "\t" + username + "\t" + password + "\t" + user_type + "\n")
+    user_file = open('User.txt', 'a') if os.path.isfile('User.txt') else open('User.txt', 'w')
+
+    user_file.write('U' + str(new_user_id) + '\t' + username + '\t' + password + '\t' + name + '\t' + user_type + '\n')
     print('User saved successfully.')
     user_file.close()
 
 
+# Create admin user in the system
 def create_admin_user():
-    admin_username = 'admin'
+    admin_default_username = 'admin'
     admin_default_password = 'password'
-    admin_password = ''
 
     print('An admin user is required for initial usage of this system.')
-    while True:
-        admin_username = input(
-            'Please enter the admin\'s username. (Default admin username: ' + admin_username + ')')
-        if string_is_blank(admin_username):
-            print('Username is required. Please try again.')
-        else:
-            break
 
-    while True:
-        admin_password = input(
-            'Please enter the admin\'s password: (Default admin password: ' + admin_default_password + ')')
-        if string_is_blank(admin_password):
-            admin_password = admin_default_password
-        else:
-            admin_confirm_password = input('Confirm the admin\'s password: ')
-            if string_is_blank(admin_confirm_password):
-                print('Please try again. Confirm the admin\'s password: ')
-            else:
-                if admin_password != admin_confirm_password:
-                    print('The passwords are not the same. Please enter the details again.')
-                else:
-                    break
+    admin_username = get_username('admin', admin_default_username)
+    admin_password = get_user_password('admin', admin_default_password)
+    admin_name = get_user_name('admin')
+
     # Create local data files
-    save_user(admin_username, admin_password, 'Admin')
+    save_user(admin_username, admin_password, admin_name, 'Admin')
+
+
+# Display UIs
+def display_welcome():
+    print('Welcome to Online Banking System.')
+
+
+def display_admin_menu():
+    selection = int(input('Here are a list that you can perform: \n' +
+                          '1. Create new customer\n' +
+                          '2. View Customer\'s Profile\n' +
+                          '3. View Customer\'s Transactions\n' +
+                          '4. Logout\n' +
+                          'Enter the number of the function to proceed.'))
+
+    # Switch case in Python.
+    # References:   https://towardsdatascience.com/switch-case-statements-are-coming-to-python-d0caf7b2bfd3
+    #               https://stackoverflow.com/questions/60208/replacements-for-switch-statement-in-python
+    match selection:
+        case 1:
+            return None
+        case 2:
+            return None
+        case 3:
+            return None
+        case 4:
+            return None
+        case _:
+            pass  # Pass. Executes nothing (Reference: https://www.javatpoint.com/python-pass)
+
+
+def display_customer_menu():
+    selection = int(input('Here are a list that you can perform: \n' +
+                          '1. Make Deposit\n' +
+                          '2. Make Withdrawal\n' +
+                          '3. View Transactions\n' +
+                          '4. Logout\n' +
+                          'Enter the number of the function to proceed.'))
+
+    match selection:
+        case 1:
+            return None
+        case 2:
+            return None
+        case 3:
+            return None
+        case 4:
+            return None
+        case _:
+            pass
 
 
 # Initialization of the program to check the user existence
 def init():
     print('Initialize first time running...')
     # Check local data files exist
-    if os.path.isfile("Users.txt") and os.path.isfile("Transaction.txt"):
+    if os.path.isfile('Users.txt') and os.path.isfile('Transaction.txt'):
+        print('Local data exists.')
+        display_welcome()
+        login()
+    else:
         create_admin_user()
         transaction_file = open('Transaction.txt', 'w')
         transaction_file.close()
-    else:
-        print('Local data exists.')
 
+
+# Register a new customer into the system
 def register_customer():
+    print('Register customer selected.')
+    customer_username = get_username('customer', None)
+    customer_password = get_user_password('customer', None)
+    customer_name = get_user_name('customer')
+
+    # Create local data files
+    save_user(customer_username, customer_password, customer_name, 'Customer')
+
 
 def login():
+    while True:
+        username = input('Please enter your username: ')
+        if string_is_blank(username):
+            print('Username is empty.')
+            continue
+        else:
+            break
 
-def deposit():
+    while True:
+        password = input('Please enter your password: ')
+        if string_is_blank(password):
+            print('Password is empty.')
+            continue
+        else:
+            break
+    # Create local data files
+    user_info = get_user_info(username, password)
 
-def withdrawal():
+    if not user_info:
+        print('Welcome, ', user_info[0])
+    else:
+        login()  # Recurring function (Reference: https://www.programiz.com/python-programming/recursion)
 
 
-# Return a list of customer names with customer ID (Dictionaries)
-def search_customer(search_term):
+# Get the info of a user in the User.txt data file.
+# Returns a list of strings. (Reference: https://stackoverflow.com/a/39397293)
+def get_user_info(username, password) -> list[str]:
+    if os.path.isfile('Users.txt'):
+        user_file = open('User.txt')
+        for line in user_file:
+            line = line.rstrip()
+            if username in line and password not in line:
+                print('Incorrect Username/password. Please try again.')
+            elif username in line and password in line:
+                # ID \t username \t password \t name \t user_type
+                # Returning ID, username and user type.
+                return [line[0], line[3], line[4]]
+            else:
+                print('User not found. Please try again.')
+        print('No user info found.')
+        return []
+    else:
+        print('No user info found.')
+        return []
 
-def view_customer_transaction(customer_id):
+
+# def deposit():
+#
+# def withdrawal():
+#
+#
+# # Return a list of customer names with customer ID (Dictionaries)
+# def search_customer(search_term):
+#
+# def view_customer_transaction(customer_id):
 
 def main():
     init()
